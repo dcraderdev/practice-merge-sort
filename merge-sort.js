2937304153ab8cf6e720e18a8076732940ed1bbb
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2022-11-01 18:01:28 -0700

    complete
    complete

--- merge-sort.js
+++ merge-sort.js
@@ -2,6 +2,10 @@
 // Do not modify the original array
 function mergeSort(arr) {
 
+
+
+
+
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
 
@@ -17,17 +21,29 @@ function mergeSort(arr) {
 
 // Takes in two sorted arrays and returns them merged into one
 function merge(arrA, arrB) {
-
-  // Create an empty return array
+console.log('arrA', arrA);
+console.log('arrB', arrB);
+  let returnArr = new Array
 
   // Point to the first value of each array
+  let aFirst = arrA[0]
+  let bFirst = arrA[0]
+  
+  // while(arrA.length && arrB.length){
+  //   if(aFirst<bFirst){
+
+  //   }
+
+
+  // }
+
   // While there are still values in each array...
     // Compare the first values of each array
     // Add the smaller value to the return array
     // Move the pointer to the next value in that array
 
   // Return the return array
-
+return returnArr
 }
 
 module.exports = [merge, mergeSort];
